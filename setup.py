08dc251f9d54359dc4b204745b17c83b430d5ff6
FlamptX <dino.shark2006@gmail.com>
2021-07-05 15:45:02 +0200

    PyPi structure

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FlamptX/mongodb-helper",
     project_urls={
-        "Python Bot": "https://python-bot.web.app",
+        "Github": "https://python-bot.web.app",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    keywords='pythonbot, api',
+    keywords='mongodb, helper, mongodbhelper',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
